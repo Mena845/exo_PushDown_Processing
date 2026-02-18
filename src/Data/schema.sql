30d6811d812ea7de907f1403bda28709cc6d7933
mena <tsikiniainatsilavina@gmail.com>
2026-02-18 16:16:50 +0300

    creation de la table invoice

--- src/Data/schema.sql
+++ src/Data/schema.sql
@@ -3,3 +3,11 @@ create database  pushdownprocessing;
 
 -- cretaion de la type enum
 CREATE TYPE invoice_status AS ENUM ('DRAFT', 'CONFIRMED', 'PAID');
+
+-- creation de la table invoice
+CREATE TABLE invoice (
+                         id SERIAL PRIMARY KEY,
+                         customer_name VARCHAR(255) NOT NULL,
+                         status invoice_status NOT NULL
+);
+
