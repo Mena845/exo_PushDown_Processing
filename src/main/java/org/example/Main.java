6446119281bb1034f86bca6cea987f9537a90a70
mena <tsikiniainatsilavina@gmail.com>
2026-02-19 10:12:15 +0300

    correction de Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,53 @@
 package org.example;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
+import java.math.BigDecimal;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String url = "jdbc:postgresql://localhost:5432/ton_db";
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/ton_database";
         String user = "postgres";
         String password = "postgres";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            DataRetriever dr = new DataRetriever(conn);
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
 
+            DataRetriever dr = new DataRetriever(connection);
+
+            // Q1
             System.out.println("Q1 - Total par facture");
             List<InvoiceTotal> totals = dr.findInvoiceTotals();
             totals.forEach(System.out::println);
 
-            System.out.println("\nQ2 - Factures CONFIRMED et PAID");
+            // Q2
+            System.out.println("\nQ2 - CONFIRMED & PAID");
             List<InvoiceTotal> confirmedPaid = dr.findConfirmedAndPaidInvoiceTotals();
             confirmedPaid.forEach(System.out::println);
 
+            // Q3
             System.out.println("\nQ3 - Totaux cumulés par statut");
             System.out.println(dr.computeStatusTotals());
 
-            System.out.println("\nQ4 - Chiffre d'affaires pondéré");
+            // Q4
+            System.out.println("\nQ4 - CA pondéré");
             System.out.println(dr.computeWeightedTurnover());
+
+            // Q5-A
+            System.out.println("\nQ5-A - HT / TVA / TTC");
+            List<InvoiceTaxSummary> taxSummaries = dr.findInvoiceTaxSummaries();
+            taxSummaries.forEach(System.out::println);
+
+            // Q5-B
+            System.out.println("\nQ5-B - CA TTC pondéré");
+            BigDecimal weightedTtc = dr.computeWeightedTurnoverTtc();
+            System.out.println(weightedTtc);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
